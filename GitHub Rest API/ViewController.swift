d971b709916b67485424a0a6dbbb42a12dd25bec
Ramil Sharapov <ramil.sharapov@mail.ru>
2022-01-12 12:35:43 -0400

    Initial Commit

---
+++ GitHub Rest API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub Rest API
+//
+//  Created by Ramil Sharapov on 2022-01-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
